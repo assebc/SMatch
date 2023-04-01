828fc6221ace45b914217fc0562c4c70f4d21fdc
bessa_ <claudioafbessa@gmail.com>
2023-04-01 09:02:52 +0100

    Starting features (#3)
    * feat: starting some features
    
    * feat: update

---
+++ frontend/src/pages/Interests/interests.js
@@ -0,0 +1,13 @@
+import * as data from "./interests.json";
+const word = data.interests;
+
+const Interests = (props) => {
+
+    
+
+    return (
+        undefined
+    );
+}
+
+export default Interests;
